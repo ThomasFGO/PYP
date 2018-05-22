d2b60ec8625d8eeedc6c869c838f564dabb55e31
Maximilien Renaut <max.renaut@gmail.com>
2018-05-22 12:15:01 +0200

    navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,13 @@
 //= require rails-ujs
 //= require_tree .
+function openNav() {
+    document.getElementById("mySidenav").style.width = "180px";
+    document.getElementById("main").style.marginLeft = "150px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
