58d63de65eebd86a135c61c78df87334c85aa67c
Claire Gautier <claire.gautier@edhec.com>
2018-05-24 14:21:19 +0200

    feed créé et index perso dans navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :friends, only: [:index, :destroy]
-  resources :friend_requests, only: [:index, :create, :update, :destroy]
-  root to: 'catches#index'
+  root to: 'feeds#index'
   devise_for :users
-  resources :catches, only: [:show, :new, :create, :update, :edit, :destroy] do
+  resources :catches, only: [:index, :show, :new, :create, :update, :edit, :destroy] do
     resources :reviews, only: [:index]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :friends, only: [:index, :destroy]
+  resources :friend_requests, only: [:index, :create, :update, :destroy]
 end
