924ab7df44318964d4ea4c4a14419e70caa69f74
Oscar Maybon <oscar.maybon@gmail.com>
2018-05-29 20:23:30 +0200

    Added snazzy map

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -158,7 +158,7 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setCenter(markers[ù0].lat, markers[0].lng);
     map.setZoom(14);
   } else {
     map.fitLatLngBounds(markers);
