6c4581c3df159f452858cc1fa2cb5b878bafa81a
Thomas Morin <thomasmorin1002@yahoo.fr>
2018-05-29 21:55:09 +0200

    add challenge_users ok

--- app/models/challenge_user.rb
+++ app/models/challenge_user.rb
@@ -1,4 +1,6 @@
 class ChallengeUser < ApplicationRecord
   belongs_to :challenge
   belongs_to :user
+
+  validates :user, uniqueness: { scope: :challenge }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ApplicationRecord
     self.friends.destroy(friend)
   end
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
