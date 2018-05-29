0da9b49ecca566af392928e59cd9f7215bca3c75
Thomas Morin <thomasmorin1002@yahoo.fr>
2018-05-29 17:55:24 +0200

    challenges index and new ok

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,6 @@
 class Challenge < ApplicationRecord
   has_many :challenge_users
+  validates :name, presence: true
+  validates :start_at, presence: true
+  validates :end_at, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_145004) do
+ActiveRecord::Schema.define(version: 2018_05_29_145306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2018_05_28_145004) do
     t.date "end_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_challenges_on_user_id"
   end
 
   create_table "friend_requests", force: :cascade do |t|
@@ -122,6 +124,7 @@ ActiveRecord::Schema.define(version: 2018_05_28_145004) do
   add_foreign_key "catches", "users"
   add_foreign_key "challenge_users", "challenges"
   add_foreign_key "challenge_users", "users"
+  add_foreign_key "challenges", "users"
   add_foreign_key "friend_requests", "users"
   add_foreign_key "friend_requests", "users", column: "friend_id"
   add_foreign_key "friendships", "users"
